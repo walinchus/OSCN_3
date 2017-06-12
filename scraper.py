d9e4548cdf4f5b494408ea6e44ef34e1deda7457
Lucia Walinchus <Lucia@professionalnewsservices.com>
2017-06-12 18:03:29 -0500

    Create scraper.py

--- scraper.py
+++ scraper.py
@@ -57,7 +57,7 @@ br.select_form(nr=0)
 print br.form
 br['db'] = ['garfield']
 br['dcct'] = ['31']
-br['FiledDateL'] = ['01/01/2011']
+br['FiledDateL'] = str(['01/01/2011'])
 print br
 response = br.submit()
 print response
